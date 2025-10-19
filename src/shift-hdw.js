110354dfddb59df1e795a45a8b3e7e651c0a612e
Sujan Parajuli <42593762+sjnprjl@users.noreply.github.com>
2025-10-19 03:38:45 -0400

    implement shift hardware

---
+++ src/shift-hdw.js
@@ -0,0 +1,26 @@
+class ShiftHardware {
+  constructor() {
+    this._lsb = 0;
+    this._msb = 0;
+    this._shiftAmount = 0;
+  }
+  write(addr, data) {
+    switch (addr) {
+      case 0x02:
+        // shift amount
+        this._shiftAmount = data & 0x07;
+        break;
+      case 0x04:
+        // shift data
+        this._lsb = this._msb;
+        this._msb = data;
+        break;
+    }
+  }
+  read(addr) {
+    switch (addr) {
+      case 0x03:
+        return ((this._msb << 8) | this._lsb) >> this._shiftAmount;
+    }
+  }
+}
